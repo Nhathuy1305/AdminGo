0208ca87eadbd39e257b13b5a00f49f866dfc69a
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-08-16 23:41:01 +0700

    search items by order func

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -39,45 +39,17 @@ func getFoods() gin.HandlerFunc {
 		startIndex := (page - 1) * recordPerPage
 		startIndex, err = strconv.Atoi(c.Query("startIndex"))
 
-		matchStage := bson.D{
-			{
-				"$match", bson.D{{}},
-			},
-		}
-		groupStage := bson.D{
-			{
-				"$group", bson.D{
-					{
-						"_id", bson.D{
-							{"_id", "null"},
-						},
-					},
-					{
-						"total_count", bson.D{
-							{"$sum", "1"},
-						},
-					},
-					{
-						"data", bson.D{
-							{"$push", "$$ROOT"},
-						},
-					},
-				},
-			},
-		}
-		projectStage := bson.D{
-			{
-				"$project", bson.D{
-					{"_id", 0},
-					{"total_count", 1},
-					{
-						"food_items", bson.D{
-							{"$slice", []interface{}{"$data", startIndex, recordPerPage}},
-						},
-					},
-				},
-			},
-		}
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		groupStage := bson.D{{
+			"$group", bson.D{{"_id", bson.D{{"_id", "null"}}},
+				{"total_count", bson.D{{"$sum", "1"}}},
+				{"data", bson.D{{"$push", "$$ROOT"}}},
+			}}}
+
+		projectStage := bson.D{{
+			"$project", bson.D{{"_id", 0}, {"total_count", 1},
+				{"food_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+			}}}
 
 		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
 			matchStage, groupStage, projectStage,
--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,30 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := userCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while listing user items"})
+			return
+		}
+
+		va
 	}
 }
 
